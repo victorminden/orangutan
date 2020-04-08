6d8fb553fbeb96005c36e8b42c419bfc66941aab
Victor Minden <victorminden@gmail.com>
2020-04-08 08:08:14 -0400

    add to object and add empty evaluator

--- src/object.rs
+++ src/object.rs
@@ -2,13 +2,17 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum Object {
+    Null,
     Integer(i64),
+    Boolean(bool),
 }
 
 impl fmt::Display for Object {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Object::Integer(int) => write!(f, "{}", int),
+            Object::Null => write!(f, "null"),
+            Object::Integer(value) => write!(f, "{}", value),
+            Object::Boolean(value) => write!(f, "{}", value),
         }
     }
 }
