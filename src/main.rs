1b8c0d938c71dc4b25cb5aacfd56892ecbddee1f
Victor Minden <victorminden@gmail.com>
2020-04-08 22:44:38 -0400

    save main

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,5 @@ extern crate orangutan;
 
 // TODO: Determine an appropriate error type to return.
 fn main() -> Result<(), std::io::Error> {
-    println!("Welcome to the Monkey programming language!");
-    println!("Feel free to type in commands to be parsed (but not yet evaluated)");
     orangutan::repl::start()
 }
