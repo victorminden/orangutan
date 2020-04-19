bf1ad5613e1ba5fdee6c3209f47692dffe6db3de
Victor Minden <victorminden@gmail.com>
2020-04-19 19:43:19 -0400

    initial form of make

--- src/code.rs
+++ src/code.rs
@@ -11,28 +11,77 @@ pub struct Definition {
     pub widths: Vec<usize>,
 }
 
-// TODO: Determine if this leads to a 1-byte Opcode we can use.
+pub enum MakeError {
+    WrongNumberOfArgs(usize),
+    WrongArgSize(usize),
+}
+
 #[derive(IntoPrimitive, TryFromPrimitive, Debug, Eq, PartialEq)]
 #[repr(u8)]
 pub enum Opcode {
     Constant,
-    OtherConstant,
 }
 
+impl Opcode {
+    pub fn definition(&self) -> Definition {
+        match self {
+            Opcode::Constant => Definition {
+                name: String::from("OpConstant"),
+                widths: vec![2],
+            },
+        }
+    }
+
+    pub fn make(self) -> Result<Instructions, MakeError> {
+        match self.definition().widths.len() {
+            0 => Ok(vec![self.into()]),
+            other => Err(MakeError::WrongNumberOfArgs(other))
+        } 
+    }
+
+    pub fn make_u16(self, operand: u16) -> Result<Instructions, MakeError> {
+        let w = &self.definition().widths;
+        if w.len() != 1 {
+            Err(MakeError::WrongNumberOfArgs(w.len()))
+        } else if w[0] != 2 {
+            Err(MakeError::WrongArgSize(w[0]))
+        } else {
+            let b = u16::to_be_bytes(operand);
+            Ok(vec![self.into(), b[0], b[1]])
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::mem::size_of;
 
     #[test]
     fn opcode_test() {
         let tests = vec![
             (0u8, Opcode::Constant),
-            (1u8, Opcode::OtherConstant),
         ];
 
+        assert_eq!(size_of::<Opcode>(), 1);
         for (input, want) in tests {
-            assert_eq!(Opcode::try_from(input), Ok(want));
+            let got = Opcode::try_from(input);
+            assert_eq!(got, Ok(want));
+        }
+    }
+
+    #[test]
+    fn make_u16_test() {
+        // Op, Operands, Expected
+        let tests = vec![
+            (Opcode::Constant, 65534u16, vec![Opcode::Constant.into(), 255u8, 254u8]),
+        ];
+
+        for (op, operand, want) in tests {
+            match op.make_u16(operand) {
+                Ok(got) => assert_eq!(got, want),
+                _ => panic!("Got error!")
+            }
         }
     }
 }
