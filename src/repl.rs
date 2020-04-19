1eb3a87bc388bf6b482486bc3bd2057115c5eb11
Victor Minden <victorminden@gmail.com>
2020-04-19 12:16:57 -0400

    simple doc comments for repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,3 +1,8 @@
+//! REPL
+//! 
+//! `repl` implements a read-evaluate-print-loop for the Monkey language.
+//! The interface is bare-bones, consisting only of reading lines of input from
+//! standard in and evaluating them, line by line.
 use crate::lexer;
 use crate::parser;
 use crate::evaluator;
@@ -21,7 +26,9 @@ const MONKEY_FACE: &str = "            __,__
            \'-----\'
 ";
 
-
+/// Starts the REPL.
+/// 
+/// Input is read line-by-line in interactive form until the user terminates the process.
 pub fn start() -> io::Result<()> {
     println!("Welcome to the Monkey programming language!");
     println!("{}", MONKEY_FACE);
