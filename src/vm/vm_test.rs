1e36604c1b6d732f3ed6c5c3cff50c8ea03f8100
Victor Minden <victorminden@gmail.com>
2020-05-03 15:37:48 -0400

    more nice closure tests

--- src/vm/vm_test.rs
+++ src/vm/vm_test.rs
@@ -360,6 +360,41 @@ fn closures_test() {
             adder(8);",
             11,
         ),
+        (
+            "let newAdderOuter = fn(a, b) {
+            let c = a + b;
+            fn(d) {
+            let e = d + c;
+            fn(f) { e + f; };
+            };
+            };
+            let newAdderInner = newAdderOuter(1, 2);
+            let adder = newAdderInner(3);
+            adder(8);",
+            14,
+        ),
+        (
+            "let a = 1;
+            let newAdderOuter = fn(b) {
+            fn(c) {
+            fn(d) { a + b + c + d };
+            };
+            };
+            let newAdderInner = newAdderOuter(2);
+            let adder = newAdderInner(3);
+            adder(8);",
+            14,
+        ),
+        (
+            "let newClosure = fn(a, b) {
+                let one = fn() { a; };
+                let two = fn() { b; };
+                fn() { one() + two(); };
+                };
+                let closure = newClosure(9, 90);
+                closure();",
+            99,
+        ),
     ];
     for (test_input, expected) in tests {
         match run(test_input) {
